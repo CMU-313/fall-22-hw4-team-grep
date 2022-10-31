ecdc0892c6d6d05217204a19978e1776b007db9d
Jimmy-Zeng-2023 <jimmyzen@andrew.cmu.edu>
2022-10-30 23:08:52 -0400

    Added one 400 test. Fixed typo in routes.py

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -13,3 +13,14 @@ def test_base_route():
 
     assert response.status_code == 200
     assert response.get_data() == b'try the predict route it is great!'
+
+def test_400_1():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/predict?age=18&absences=21&health=hello”'
+
+    response = client.get(url)
+
+    assert response.status_code == 400
+
