2006bbfaf6f5b42cf5651ad345cc093351fba20f
spham67 <98799078+spham67@users.noreply.github.com>
2022-11-08 02:58:13 -0500

    Updating predict route and tests and API documentation (#5)
    Implemented prediction route using new features, updated tests to use
    new features and handle 404 errors correctly, and updated the API
    documentation by removing 404 errors.
    
    Co-authored-by: Sean Pham <seanpham@Seans-MacBook-Pro-2.local>

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -19,12 +19,12 @@ def test_success_predict_route():
     configure_routes(app)
     client = app.test_client()
     url ='/predict'
-    request_data_1 = {'G1': 15, 'G2': 12, 'failures' : 0, 'studytime' : 4, 'absences' : 1}
+    request_data_1 = {'G1': 20, 'G2': 20, 'failures' : 0, 'studytime' : 4, 'absences' : 1}
     response_1 = client.get(url, query_string= request_data_1)
 
     # returns 200 response with succesful admission status (1) 
     assert response_1.status_code == 200
-    assert response_1.get_data() == {b'1', b'15', b'12', b'0', b'4', b'1'}
+    assert response_1.get_data() == b'{"AdmissionStatus":1,"G1":20,"G2":20,"absences":1,"failures":0,"studytime":4}\n'
 
 
     # returns 200 response with failure admission status (0)     
@@ -32,48 +32,52 @@ def test_success_predict_route():
     response_2 = client.get(url, query_string= request_data_2)
 
     assert response_2.status_code == 200
-    assert response_2.get_data() == {b'0', b'3', b'3', b'4', b'1', b'7'}
+    assert response_2.get_data() == b'{"AdmissionStatus":0,"G1":3,"G2":3,"absences":7,"failures":4,"studytime":1}\n'
 
 
 def test_invalid_argument_type():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?age=18&absences=21&health=hello'
+    url1 = '/predict?G1=0&G2=hello&failures=0&studytime=4&absences=0'
 
-    response = client.get(url)
+    response1 = client.get(url1)
 
     # returns 400 response with invalid argument type
-    assert response.status_code == 400
+    assert response1.status_code == 400
+    assert response1.get_data() == b'{"Error":"Missing inputs or inputs invalid!"}\n'
+
+
+    # returns 400 response with invalid argument type
+    url2 = '/predict?G1=0&G2=2&failures=asdf&studytime=asdf&absences=0'
+    response2 = client.get(url2)
+    assert response2.status_code == 400
+    assert response2.get_data() == b'{"Error":"Missing inputs or inputs invalid!"}\n'
+
 
 def test_missing_inputs():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?age=18'
+    url = '/predict?G1=0&G2=hello&failures=0&studytime=4'
 
     response = client.get(url)
 
     # returns 400 response with missing inputs
     assert response.status_code == 400
+    assert response.get_data() == b'{"Error":"Missing inputs or inputs invalid!"}\n'
+
 
 def test_improperly_formatted_inputs():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?age=18absences=6G1=3G2=5studytime=3'
+    url = '/predict?failures=0&G1=0&G2=hello&studytime=4&absences=0'
 
     response = client.get(url)
 
     # returns 400 response with improperly formatted inputs
     assert response.status_code == 400
+    assert response.get_data() == b'{"Error":"Missing inputs or inputs invalid!"}\n'
+
     
-def test_400_2():
-    app = Flask(__name__)
-    configure_routes(app)
-    client = app.test_client()
-    url = '/%predict?age=blah&absences=2&health=3'
-    
-    response = client.get(url)
-    
-    assert response.status_code == 400
